21b10a1b347ffd2996ed21e64801bb197031b890
Morten Jensen <mo_ten@hotmail.com>
2025-03-12 15:41:11 +0100

    Task2
    Completed Task2

---
+++ Task2/src/Cafe.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Cafe {
+    //2.c create class cafe with ArrayList coffeeMenu and getList() method
+    private ArrayList<String> coffeeMenu;
+
+    public Cafe() {
+        this.coffeeMenu = new ArrayList<>();
+    }
+
+    public ArrayList<String> getCoffeeMenu(){
+        return coffeeMenu;
+    }
+
+    //2.c make a method loadData()
+    public void loadMenuData() {
+        //2.d read the file using a scanner object
+        try {
+            File file = new File("data/coffees.txt");
+            Scanner sc = new Scanner(file);
+            //2.e Loop trough file and add to the lines to coffeeMenu
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                coffeeMenu.add(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Check path or filename");
+        }
+    }
+}
---
+++ Task2/src/Main.java
@@ -0,0 +1,16 @@
+//2.b create a class Main and a main method
+public class Main {
+    public static void main(String[] args) {
+
+        //2.g create a instance of Cafe class and call the loadMenuDate()
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
+
+        //2.h print alle the elements in the list coffeeMenu
+        System.out.println("Welcome to Java Beans Café! Here is our menu:");
+        for(String coffee: cafe.getCoffeeMenu()){
+            System.out.println(coffee);
+        }
+
+    }
+}

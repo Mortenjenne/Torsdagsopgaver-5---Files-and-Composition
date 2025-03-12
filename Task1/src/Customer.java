9d52d2ef097dceaa295dd1095ebcfad74cf7a0d9
Morten Jensen <mo_ten@hotmail.com>
2025-03-12 15:07:10 +0100

    Task1
    Completed

---
+++ Task1/src/Customer.java
@@ -0,0 +1,50 @@
+public class Customer {
+    //1.a Lav en klasse Customer
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private int id;
+    private static int counter = 0;
+
+    //1.b Lav en konstruktør der tager kundens navn og brugernavn som parameter
+    Customer(String firstName, String lastName, String userName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        //1.c counter tælles op med en hver gang konstruktøren bliver kaldt og brug counter til at initialisere id
+        counter ++;
+        this.id = counter;
+    }
+
+    //1.d lav toString + getters + setters
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        String s =  "Customer ID: " + this.id + ", Name: " + this.firstName + " " + this.lastName + ", Username: " + this.userName;
+        return s;
+    }
+}
---
+++ Task1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //1.e Opret en Arraylist som kaldes customer
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        customers.add(new Customer("Chris","Prat","Star-Lord"));
+        customers.add(new Customer("Mads","Mikkelsen","Le Chiffre"));
+        customers.add(new Customer("Viggo","Mortensen","Aragorn"));
+        customers.add(new Customer("Bradley","Cooper","Rocket Racoon"));
+
+        printCustomer(customers);
+    }
+    //1.f Lav en static metode printCustomers der printer alle kunder ud
+    public static void printCustomer(ArrayList<Customer> customers) {
+        for(Customer c: customers) {
+            System.out.println(c);
+        }
+    }
+}
